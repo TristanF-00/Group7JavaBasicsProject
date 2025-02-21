8fd61f662309c5edc0900201961a8977b11c1648
TristanF-00 <tfsaypharath@gmail.com>
2025-02-21 01:20:51 -0500

    Second Commit

--- src/tristanFernandez/CT1.java
+++ src/tristanFernandez/CT1.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class CT1 {
     public static void main(String[] args) {
-
         /* Create a program that uses an array to store a list of temperatures for a week,
         and then uses a loop to find the highest and the lowest temperature for the week.
          */
